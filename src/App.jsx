2df40a6b8ee4c7d2b3be5c085c61623a10efa3dd
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-06-12 19:40:43 -0500

    feat: 🎸 Lógica del tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,62 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 const TURNS = {
-  X: 'x',
-  O: 'o',
-}
+  X: "x",
+  O: "o",
+};
 
-const Square = ({ children, updateBoard, index }) => {
-  return <div className='square' key={index}> {children} </div>
-}
+const Square = ({ children, isSelected, updateBoard, index }) => {
 
-function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurns] = useState(TURNS.X)
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
 
   return (
-    <main className='board'>
+    <div onClick={handleClick} className={className} key={index}>
+      {children}
+    </div>
+  );
+};
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn // X ó O
+    setBoard(newBoard)
+
+    // debemos asegurarnos de cambiar el turno entre uno y otro
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn)
+  }
+
+  return (
+    <main className="board">
       <h1>Triqui</h1>
-      <section className='game'>
-        {
-          board.map((_, index) => {
-            return(
-                <Square
-                  key={index}
-                  index={index}
-                  >
-                    {board[index]}
-                </Square>
-            )
-          })
-        }
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            // ¿ por qué se pasa la función y no la ejecución ?
+            // porque no quiero ejecutar la función (ejemplo: updateBoard()); en este caso si hago esto, se me ejecutará 9 veces al recargar, y debo ejecutarlo solo cuando lo necesite
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
